10ab371c4d634c4a2bb4c0aad2492a0bc06afae8
Juliana Lobo <sousalobojuliana@gmail.com>
2022-09-15 20:21:54 -0300

    Primeiro commit
    Terminando de configurar o controlador da página principal

--- src/main/java/com/professorangoti/bootstrap/HomeController.java
+++ src/main/java/com/professorangoti/bootstrap/HomeController.java
@@ -1,5 +1,12 @@
 package com.professorangoti.bootstrap;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
-    
+    @GetMapping("home")
+    public String aula(){
+        return "home";
+    }
 }
